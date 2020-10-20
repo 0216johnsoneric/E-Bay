b54b2374c7da824277b21539d64edf2cdb1132d0
Eric Johnson <ericjohnson0216@icloud.com>
2020-10-19 22:30:43 -0400

    Done.

--- index.js
+++ index.js
@@ -18,8 +18,30 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
-  createProduct();
+//   createProduct();
+    start();
 });
+
+function start() {
+  inquirer
+    .prompt({
+      name: "postOrBid",
+      type: "list",
+      message: "Would you like to [POST] an auction or [BID] on an auction?",
+      choices: ["POST", "BID", "EXIT"]
+    })
+    .then(function(answer) {
+      // based on their answer, either call the bid or the post functions
+      if (answer.postOrBid === "POST") {
+        postAuction();
+      }
+      else if(answer.postOrBid === "BID") {
+        bidAuction();
+      } else{
+        connection.end();
+      }
+    });
+}
 
 function createProduct() {
   console.log("Inserting a new item...\n");
